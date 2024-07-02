ba976ed5ca31986a7892f178a00b2f23044f276b
Anshu0105 <anshumanmishra0105@gmail.com>
2024-07-02 20:59:00 +0530

    Add files via upload

---
+++ demo/1_Home🏠.py
@@ -0,0 +1,24 @@
+import streamlit as st
+# a,space,b=st.columns(1,4,1)
+# st.image("pic.png")
+st.header("Anshuman Mishra")
+st.subheader("AI | ML freshman🤖")
+st.write("I am a student of AI and ML at CV Raman Global University📚")
+st.markdown("---")
+st.image("pic.png")
+st.markdown("---")
+st.subheader("About🗒️")
+p="""
+I'm Anshuman Mishra, thrilled to embark on my journey as a freshman in Computer Science Engineering, specializing in Artificial Intelligence and Machine Learning at CV Raman Global University, Bhubaneswar.
+
+With a deep-seated passion for the cutting-edge realms of AI and ML, I'm on a quest to dive headfirst into the exciting developments shaping our future.
+
+Currently immersing myself in coursework, projects, and self-study, I'm committed to unraveling the mysteries behind AI and ML concepts, algorithms, and their real-world applications.
+
+Excited to channel my education and enthusiasm towards tackling tangible challenges and carving out meaningful contributions in the realm of AI and ML.
+
+Eager to explore opportunities for internships, projects, and collaborations that not only hone my skills but also foster impactful innovation.
+"""
+
+st.write(p)
+st.toast("Welcome To My Profile 🤝")
---
+++ demo/pages/1_Education.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.header("Education📕")
+st.divider()
+
+st.toast("My Education")
+st.header("College🏫")
+st.subheader("C.V Raman global University , Bhubaneshwar,Odisha")
+st.write("Bachelors in Technology in AIML(CSE)")
+st.markdown("---")
+
+st.header("Secondary School🏫")
+st.subheader("Narayana Junior College, Dombivli, Maharashtra")
+st.write("Grade : 70.5%")
+st.markdown("---")
+
+st.header("School🏫")
+st.subheader("Omkar International School, Dombivli, Maharashtra")
+st.write("Grade : 84.7%")
+st.markdown("---")
